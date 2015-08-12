afeaaa28e720c0ef61dfed581b029b64ba8d1dd6
heymysweetheart <zhixiangjin@126.com>
2015-08-12 10:43:49 +0800

    add stream close method

--- javabasic/JavaIO/FileInputStreamFileOutputStream.java
+++ javabasic/JavaIO/FileInputStreamFileOutputStream.java
@@ -7,19 +7,30 @@ import java.io.IOException;
  * Created by leo on 15/8/12.
  */
 public class FileInputStreamFileOutputStream {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         FileInputStream fileInputStream = null;
         FileOutputStream fileoutputStream = null;
         int tempInt;
 
-        fileInputStream = new FileInputStream("/Users/leo/sia.log");
-        fileoutputStream = new FileOutputStream("/Users/leo/sia.log.bak");
+        try {
+            fileInputStream = new FileInputStream("/Users/leo/sia.log");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            fileoutputStream = new FileOutputStream("/Users/leo/sia.log.bak");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
         try {
             while ((tempInt = fileInputStream.read()) != -1) {
                 fileoutputStream.write(tempInt);
                 System.out.print((char) tempInt);
             }
+
+            fileInputStream.close();
+            fileoutputStream.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
